20e3402887f70af8cfa2c9e1219c2df4f04ca106
Shital Shankarlal Nishad <102948948+shital4197@users.noreply.github.com>
2025-02-15 20:22:52 +0530

    Add files via upload

---
+++ CustmoersMain.java
@@ -0,0 +1,14 @@
+package com.jdk;
+
+public class CustmoersMain {
+	
+	public static void main(String[] args) throws Exception{
+		
+		Custmoers m1 = new Custmoers();
+//		m1.insertData();
+//		m1.updateData();
+//		m1.deleteData();
+		m1.getAllData();
+	}
+	
+}
